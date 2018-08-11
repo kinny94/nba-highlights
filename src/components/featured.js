f07021aa4fa11fa9b2933f73e7acf839b6de47b2
kinny94 <ad.sam007@gmail.com>
2018-08-10 21:54:07 -0400

    Hosting fjson file and getting data

---
+++ src/components/featured.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Featured = ( props ) => {
+    console.log( props );
+    return(
+        <div>
+            slide
+        </div>
+    )
+}
+
+export default Featured;
--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
+import Featured from './featured';
+
+const URL = "https://api.myjson.com/bins/11rzz8";
 class Home extends Component{
+
+   
+    constructor( props ){
+        super( props );
+        this.state = {
+            home: ''
+        }
+    }
+
+    componentDidMount(){
+        fetch( URL, { method: 'GET' }).then( response => { 
+            return response.json();
+        }).then( json => {
+            this.setState({
+                home: json.home
+            })
+        });
+    }
+
     render(){
         return(
             <div>
-                Home
+                <Featured slides={ this.state.home.slider } />
             </div>
         )
     }
