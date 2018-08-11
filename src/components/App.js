864c335d2049a6ff15455809932ffb72bed951b2
kinny94 <ad.sam007@gmail.com>
2018-08-10 20:04:29 -0400

    setting up boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Componet } from 'react';
+
+class App extends Component{
+    render(){
+        return(
+            <div>Hello World!</div>
+        );
+    }
+}
+
+export default App;
