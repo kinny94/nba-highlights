2b505b58d2b024666ab96aa8d2e1fdb8113dd9ce
kinny94 <ad.sam007@gmail.com>
2018-08-13 22:00:25 -0400

    Working on slider

--- src/components/featured.js
+++ src/components/featured.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import Slider from 'react-slick';
+
+const settings = {
+    arrows: false,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+}
+
+const generateSlides = ({ slides }) => {
+    if( slides ){
+        return(
+            <Slider { ...settings }>
+                { 
+                    slides.map(( item ) => {
+                        return(
+                            <div key={ item.id } style={ {background: `url(/images/covers/${ item.cover })`}} className="item-slider" >
+                                <div className="caption">
+                                    <h4>{ item.topic }</h4>
+                                    <p>{ item.title }</p>
+                                </div>
+                            </div>
+                        )
+                    }) 
+                }
+            </Slider>
+        )
+    }
+}
 
 const Featured = ( props ) => {
     console.log( props );
     return(
         <div>
-            slide
+            { generateSlides( props ) }
         </div>
     )
 }
